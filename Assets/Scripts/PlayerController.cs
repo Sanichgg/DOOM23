027ebdeb5dceaef4b23983dffe3121f1d3d6eb12
Sanich <Sanichgg@yandex.ru>
2023-09-18 18:07:58 +0300

    Movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float forwardSpeed = 1;
+    [SerializeField] float sideSpedd = 1;
+
+    Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rb.AddForce (new Vector3(horizontal * Time.fixedDeltaTime * sideSpedd, 
+            0,
+            vertical * Time.fixedDeltaTime * forwardSpeed));
+    }
+}
