5de259aef85e839c056b3a94f24af7ffe410aba6
Sanich <Sanichgg@yandex.ru>
2023-10-16 15:48:47 +0300

    HoTFix2

--- Assets/Scripts/LavaScript.cs
+++ Assets/Scripts/LavaScript.cs
@@ -11,16 +11,21 @@ public class LavaScript : MonoBehaviour
     {
         if(collision.gameObject.GetComponent<DamagebleComponent>())
         {
+            player = collision.gameObject.GetComponent<DamagebleComponent>();
             StartCoroutine(TakeDamagePerTime(damageDeal, player));
+            
         }
     }
     public void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.GetComponent<DamagebleComponent>()) StopAllCoroutines();
+        if (collision.gameObject.GetComponent<DamagebleComponent>())
+            player = collision.gameObject.GetComponent<DamagebleComponent>();
+        StopAllCoroutines();
     }
 
     IEnumerator TakeDamagePerTime(int damage, DamagebleComponent playerStats)
     {
+        
         while (true)
         {
             playerStats.DealDamage(damage);
